fb80b7d2f32ebd9f612c26e9ddbd87a4320e899a
GaborHankoczi <hankoczigabor@gmail.com>
2023-05-04 19:09:42 +0200

    Firmware: IO fix

--- Firmware/login-controller/components/io/io.c
+++ Firmware/login-controller/components/io/io.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "io.h"
+#include "driver/gpio.h"
 
-#define DIP_SWITCH_1 GPIO_NUM_36
-#define DIP_SWITCH_2 GPIO_NUM_39
-#define DIP_SWITCH_3 GPIO_NUM_34
-#define DIP_SWITCH_4 GPIO_NUM_35
-#define DIP_SWITCH_5 GPIO_NUM_16
-#define DIP_SWITCH_6 GPIO_NUM_4
-#define DIP_SWITCH_7 GPIO_NUM_21
-#define DIP_SWITCH_8 GPIO_NUM_2
+#define DIP_SWITCH_1 36//GPIO_NUM_36
+#define DIP_SWITCH_2 39//GPIO_NUM_39
+#define DIP_SWITCH_3 34//GPIO_NUM_34
+#define DIP_SWITCH_4 35//GPIO_NUM_35
+#define DIP_SWITCH_5 16//GPIO_NUM_16
+#define DIP_SWITCH_6 4//GPIO_NUM_4
+#define DIP_SWITCH_7 21//GPIO_NUM_21
+#define DIP_SWITCH_8 2//GPIO_NUM_2
 
 
 uint8_t io_get_station_number(void){
